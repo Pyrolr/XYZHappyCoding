301fface9a8e33086a93d37091d5472ba5efc464
Hardik <adolfhardik@gmail.com>
2016-02-15 21:52:12 +0530

    Added build failed and build started updates

--- XYZPlugin/NSObject_Extension.swift
+++ XYZPlugin/NSObject_Extension.swift
@@ -13,18 +13,30 @@ extension NSObject {
     class func pluginDidLoad(bundle: NSBundle) {
         let appName = NSBundle.mainBundle().infoDictionary?["CFBundleName"] as? NSString
         if appName == "Xcode" {
-        	if sharedPlugin == nil {
-        		sharedPlugin = XYZPlugin(bundle: bundle)
-        	}
+            if sharedPlugin == nil {
+                sharedPlugin = XYZPlugin(bundle: bundle)
+            }
         }
     }
     
     func xyz_initWithIcon(icon: AnyObject?, message: String, parentWindow: AnyObject, duration: Double) -> AnyObject? {
-        if icon != nil && XYZTogglingHandler.isEnabled() && message.containsString("Succeeded") {
-            let imageIcon = NSBundle(identifier: "com.kietnguyen.XYZPlugin")!.imageForResource(XYZFaceFactory.createFace())
-            self.xyz_initWithIcon(imageIcon!, message: message, parentWindow: parentWindow, duration: duration)
+        
+        if icon != nil && XYZTogglingHandler.isEnabled() {
+            var imageIcon:NSImage?
+            if message.containsString("Succeeded") {
+                imageIcon = NSBundle(identifier: "com.kietnguyen.XYZPlugin")!.imageForResource(XYZFaceFactory.createFace("happy"))
+                self.xyz_initWithIcon(imageIcon!, message:"Yay, Build Succeeded 👍", parentWindow: parentWindow, duration: duration)
+            }else if message.containsString("Failed") {
+                imageIcon = NSBundle(identifier: "com.kietnguyen.XYZPlugin")!.imageForResource(XYZFaceFactory.createFace("sad"))
+                self.xyz_initWithIcon(imageIcon!, message:"Sorry, Some errors in build ✋", parentWindow: parentWindow, duration: duration)
+            }else if message.containsString("Started") {
+                imageIcon = NSBundle(identifier: "com.kietnguyen.XYZPlugin")!.imageForResource(XYZFaceFactory.createFace("build"))
+                self.xyz_initWithIcon(imageIcon!, message:"Making your thoughts come alive", parentWindow: parentWindow, duration: duration)
+            } else {
+                self.xyz_initWithIcon(icon, message: message, parentWindow: parentWindow, duration: duration)
+            }
             
-            if self.isKindOfClass(NSPanel) {
+            if self.isKindOfClass(NSPanel) && imageIcon != nil {
                 let panel = self as! NSPanel
                 if panel.contentView!.isKindOfClass(NSVisualEffectView) {
                     let visualEffectView = panel.contentView as! NSVisualEffectView
--- XYZPlugin/XYZFaceFactory.swift
+++ XYZPlugin/XYZFaceFactory.swift
@@ -10,10 +10,24 @@ import AppKit
 
 class XYZFaceFactory: NSObject {
     
-    static let faceNames = ["image0", "image1", "image2", "image3", "image4"]
+    static let happyFaceNames = ["happy0", "happy1", "happy2", "happy3", "happy4"]
+    static let sadFaceNames = ["sad0", "sad1", "sad2", "sad3", "sad4"]
+    static let buildFaceNames = ["build0", "build1", "build2", "build3"]
     
-    class func createFace() -> String {
-        let index = Int(arc4random_uniform(UInt32(faceNames.count)))
-        return faceNames[index]
+    class func createFace(faceType:String) -> String {
+        switch(faceType) {
+        case "happy":
+            let index = Int(arc4random_uniform(UInt32(happyFaceNames.count)))
+            return happyFaceNames[index]
+            
+        case "sad":
+            let index = Int(arc4random_uniform(UInt32(sadFaceNames.count)))
+            return sadFaceNames[index]
+        case "build":
+            let index = Int(arc4random_uniform(UInt32(buildFaceNames.count)))
+            return buildFaceNames[index]
+        default:
+            return ""
+        }
     }
 }
